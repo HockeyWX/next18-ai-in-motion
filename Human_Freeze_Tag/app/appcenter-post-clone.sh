8d9fac7f5797ab3feb1f7ad416d7f5b51893de55
Gavin Williams <611832+gavinwilliams@users.noreply.github.com>
2019-05-19 09:04:21 +0100

    Catting

--- Human_Freeze_Tag/app/appcenter-post-clone.sh
+++ Human_Freeze_Tag/app/appcenter-post-clone.sh
@@ -4,3 +4,4 @@ echo $APPCENTER_SOURCE_DIRECTORY
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 echo $FIREBASE_KEY | base64 -D  > $APPCENTER_SOURCE_DIRECTORY/Human_Freeze_Tag/app/google-services.json
+cat $APPCENTER_SOURCE_DIRECTORY/Human_Freeze_Tag/app/google-services.json
